a08a3575fb6cad79155397b31aa0e516167ebc36
afikri99 <afikri99@gmail.com>
2026-07-09 09:14:00 +0700

    samain model dengan nama atribut

--- laravel/app/Models/AsetModel.php
+++ laravel/app/Models/AsetModel.php
@@ -7,12 +7,51 @@ use Illuminate\Database\Eloquent\Model;
 class AsetModel extends Model
 {
     protected $table = 'assets';
+    
     protected $fillable = [
         'nama_aset',
         'harga',
         'satuan',
         'stok',
         'supplier_from',
+        'status',
     ];
-    
+
+    protected $casts = [
+        'harga' => 'integer',
+        'stok' => 'integer',
+        'status' => 'integer',
+    ];
+
+    /**
+     * Relasi dengan QuotationItemMaterial
+     */
+    public function quotationItemMaterials()
+    {
+        return $this->hasMany(QuotationItemMaterial::class, 'asset_id');
+    }
+
+    /**
+     * Relasi dengan SalesOrderItemMaterial
+     */
+    public function salesOrderItemMaterials()
+    {
+        return $this->hasMany(SalesOrderItemMaterial::class, 'asset_id');
+    }
+
+    /**
+     * Relasi dengan ProductionMaterial
+     */
+    public function productionMaterials()
+    {
+        return $this->hasMany(ProductionMaterial::class, 'asset_id');
+    }
+
+    /**
+     * Relasi dengan DeliveryOrderItemMaterial
+     */
+    public function deliveryOrderItemMaterials()
+    {
+        return $this->hasMany(DeliveryOrderItemMaterial::class, 'asset_id');
+    }
 }
--- laravel/app/Models/ClientModel.php
+++ laravel/app/Models/ClientModel.php
@@ -7,13 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ClientModel extends Model
 {
     protected $table = 'clients';
+    
     protected $fillable = [
         'id_perusahaan',
         'nama_perusahaan',
         'email_perusahaan',
         'nama_kontak_perusahaan',
-        'attn',
-        'cc',
         'npwp_perusahaan',
         'alamat_pengiriman_perusahaan',
         'nomor_telepon_pengiriman',
@@ -23,4 +22,36 @@ class ClientModel extends Model
         'nomor_rekening_perusahaan',
         'created_by',
     ];
+
+    /**
+     * Relasi dengan Quotation
+     */
+    public function quotations()
+    {
+        return $this->hasMany(Quotation::class, 'client_id');
+    }
+
+    /**
+     * Relasi dengan SalesOrder
+     */
+    public function salesOrders()
+    {
+        return $this->hasMany(SalesOrder::class, 'client_id');
+    }
+
+    /**
+     * Relasi dengan DeliveryOrder
+     */
+    public function deliveryOrders()
+    {
+        return $this->hasMany(DeliveryOrder::class, 'client_id');
+    }
+
+    /**
+     * Relasi dengan Invoice
+     */
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class, 'client_id');
+    }
 }
